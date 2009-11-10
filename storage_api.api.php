9263dd2a688e93303c13df1d7c3bc9c2c74c016b
Jonathan Brown <jbrown@bluedroplet.com>
2009-11-10 02:39:17 +0000

    refactoring generate file during its own request serve_url caching file entity caching api changes, including single storage_api_file_add() function serving access control in menu system callback

---
+++ storage_api.api.php
@@ -0,0 +1,24 @@
+<?php
+// $Id$
+
+/**
+* @file
+* Hooks provided by Storage API.
+*/
+
+/**
+* @addtogroup hooks
+* @{
+*/
+
+/**
+ * Generate a file.
+ *
+ * @param $file
+ *   The file to be generated.
+ * @return
+ *   Filepath to the generated file. It will be deleted.
+ */
+function hook_storage_file_generate($file) {
+}
+
