3d2b2c2247cc955ce731755860111d8ef955ad81
kimpepper <kimpepper@370574.no-reply.drupal.org>
2014-12-22 12:52:48 +0100

    Issue #2396981 by kim.pepper: Use Drupal coding standards

--- audit/storage_audit.api.php
+++ audit/storage_audit.api.php
@@ -4,29 +4,28 @@
  * @file
  * Hooks provided by Storage Audit.
  */
+
 /**
  * @addtogroup hooks
- * @{
  */
 
 /**
  * Determine if extra files in this container should be imported.
  *
- * @param $container
+ * @param \StorageContainer $container
  *   The container being audited.
- * @return
+ *
+ * @return string
  *   ID of selector extra files should become owned by.
  */
 function hook_storage_audit_import_container(StorageContainer $container) {
-  
 }
 
 /**
  * Alert module to a file having been imported.
  *
- * @param $storage
+ * @param \Storage $storage
  *   Storage of the imported file.
  */
 function hook_storage_audit_import(Storage $storage) {
-  
 }
